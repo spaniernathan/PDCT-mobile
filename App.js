cda3a92738a548a3301c42ea06765d6bc345026a
Redha Kabouia <redha.kabouia@epitech.com>
2021-02-11 11:25:22 +0100

    Can take picture

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Camera from './components/Camera';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Camera />
-      <StatusBar style='auto' />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    image: undefined,
+  };
+
+  selectPicture = async () => {
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+      aspect: [4,3],
+      allowsEditing: true,
+    });
+    if (!cancelled) this.setState({ image: uri });
+  };
+
+  takePicture = async () => {
+    await Permissions.askAsync(Permissions.CAMERA);
+    const { cancelled, uri } = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+    });
+    this.setState({ image: uri });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: this.state.image }} />
+        <View style={styles.row}>
+          <Button onPress={this.selectPicture}>Gallery</Button>
+          <Button onPress={this.takePicture}>Camera</Button>
+        </View>
+      </View>
+    );
+  }
 }
 
+const Button = ({ onPress, children }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.text}>{children}</Text>
+  </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 21,
+  },
+  row: { flexDirection: 'row' },
+  image: { width: 300, height: 300, backgroundColor: 'gray' },
+  button: {
+    padding: 13,
+    margin: 15,
+    backgroundColor: '#dddddd',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
 });
