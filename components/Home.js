f80db1e5046f3ecdbfe4c411b5ab4e840d03879e
Redha Kabouia <redha.kabouia@epitech.com>
2021-05-07 13:44:18 +0200

    Add connection with the backend. Store image in firebase

--- components/Home.js
+++ components/Home.js
@@ -2,35 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
+import ApiKeys from '../database/firebase';
+import * as firebase from 'firebase';
 
 export default class Home extends React.Component {
-  state = {
-    image: undefined,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: undefined,
+    }
 
+    // Initialize firebase...
+    if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FireBaseConfig); }
+  }
   selectPicture = async () => {
     await Permissions.askAsync(Permissions.CAMERA_ROLL);
     const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
       aspect: [4,3],
       allowsEditing: true,
     });
-    console.log(uri);
     if (!cancelled) this.setState({ image: uri });
-
-    // let response = await fetch(
-    //   'BASE_URL' + '/api/upload', {
-    //     method:'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //       file: uri
-    //     }),
-    //   }
-    // );
-
-    // let responseJson = await response.json();
-    // console.log(responseJSon);
   };
 
   takePicture = async () => {
@@ -41,6 +32,37 @@ export default class Home extends React.Component {
     this.setState({ image: uri });
   };
 
+  uploadPicture = async () => {
+     const data = new FormData();
+  
+     data.append("file", {uri: Platform.OS === "android" ? this.state.image : this.state.image.replace("file://", ""), name: "test.jpg"});
+
+     fetch("http://trackyourspot.millen.se/api/upload", {
+       method: "POST",
+       headers: {
+         'Content-Type': 'multipart/form-data'
+       },
+       body: data
+     }).then(response => response.json())
+       .then(response => {
+         console.log("upload succes", response);
+         alert("Upload success!");
+         this.uploadImageOnFireBase(this.state.image, "test-image");
+         this.setState({ photo: null });
+       })
+       .catch(error => {
+         console.log("upload error", error);
+         alert("Upload failed!");
+       });
+  };
+
+  uploadImageOnFireBase = async (uri, imageName) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    var ref = firebase.storage().ref().child("images/" + imageName);
+    return ref.put(blob);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -48,6 +70,7 @@ export default class Home extends React.Component {
         <View style={styles.row}>
           <Button onPress={this.selectPicture}>Gallery</Button>
           <Button onPress={this.takePicture}>Camera</Button>
+          <Button onPress={this.uploadPicture}>Upload</Button>
         </View>
       </View>
     );
---
+++ database/firebase.js
@@ -0,0 +1,11 @@
+export default {
+      FireBaseConfig: {
+            apiKey: "AIzaSyB1pBh0xEGYvRYffdYlK_6jcIyym2Qi_4I",
+            authDomain: "pdct-mobile.firebaseapp.com",
+            projectId: "pdct-mobile",
+            storageBucket: "pdct-mobile.appspot.com",
+            messagingSenderId: "798159847402",
+            appId: "1:798159847402:web:18b640020b51fe25d93c93",
+            measurementId: "G-MWPJ52FWXZ"
+        }
+  };
